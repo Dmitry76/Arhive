fffa7e95fee507393b7764741770ce7fe773ce84
Asus <dimetry76@gmail.com>
2021-08-07 16:15:08 +0200

    111

---
+++ test.py
@@ -0,0 +1,8 @@
+print('Hello')
+a = input('введите ряд чисел через пробел: ').split()
+first_a = a[0]
+last_a = a[len(a)-1]
+a[len(a)-1] = a[0]
+a[0] = last_a
+a.append(int(first_a) + int(last_a))
+print(a)
